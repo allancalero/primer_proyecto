b3849dabd9d42f3ebd2bf3fdc3f4f79ee9c61997
Allan Calero <allan.calero2gmail.com>
2023-09-23 11:44:01 -0600

    java primera clases int

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log ("Hello World")
+console.log ("Hello World");
+
+public class Mi_Clase_1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
